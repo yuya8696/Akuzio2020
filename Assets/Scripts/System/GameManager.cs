f4f45dd4d8ff5d88d52348e6adba8cf83836293a
1134142015609 <1134142015609@nsl.ad.nec.co.jp>
2021-04-05 09:45:43 +0900

    first commit

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveSceneManager))]
+[RequireComponent(typeof(SaveManager))]
+[RequireComponent(typeof(SoundManager))]
+[DefaultExecutionOrder(-5)]
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+
+	[Header("シーンロード時に自動生成するプレハブを登録")]
+	[SerializeField]
+	GameObject[] prefabs = null;
+
+	MoveSceneManager moveSceneManager;
+	SaveManager saveManager;
+	SoundManager soundManager;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		if (Debug.isDebugBuild)
+		{
+			
+		}
+
+		moveSceneManager = GetComponent<MoveSceneManager>();
+		saveManager = GetComponent<SaveManager>();
+		soundManager = GetComponent<SoundManager>();
+	}
+
+	void Start()
+	{
+		if (Debug.isDebugBuild)
+		{
+			InstantiateWhenLoadScene();
+		}
+	}
+
+	void Update()
+	{
+        
+	}
+
+	public void InstantiateWhenLoadScene()
+	{
+		if(moveSceneManager.SceneName == "Title")
+		{
+			return;
+		}
+
+		foreach (GameObject prefab in prefabs)
+		{
+			Instantiate(prefab, transform.position, Quaternion.identity);
+		}
+	}
+
+}
+
+
